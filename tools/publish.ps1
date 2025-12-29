8724b08b227592407237d93acbddacd388ea9320
Georgia-Antoniou <georgiaantoniou96@gmail.com>
2025-12-29 20:25:17 +0000

    Update Deployment tool and add deployment to solution.

--- tools/publish.ps1
+++ tools/publish.ps1
@@ -8,6 +8,9 @@ if([string]::IsNullOrWhitespace($RootPath))
     $RootPath = "C:\Users\Georgia\source\repos\";
 }
 
+$commitHash = (& git log -1 --pretty=format:"%H")
+$commitMessage = (& git log -1 --pretty=format:"%s")
+
 $tempFolder = "C:\temp\georgia-antoniou\publish";
 
 Remove-Item $tempFolder -Recurse -Force
@@ -23,3 +26,13 @@ Copy-Item -Path $tempWwwRoot  -Destination $githubWebSite -Recurse -Force
 
 
 Copy-Item -Path "$githubWebSite\index.html" -Destination "$githubWebSite\404.html" -Force
+
+$originalFolder = $pwd
+
+Set-Location $githubWebSite
+git add . 
+$deployMessage = "Deploy " + (Get-Date) + [Environment]::NewLine + "Hash: $commitHash" + [Environment]::NewLine + "Message: $commitMessage";
+git commit -m $deployMessage
+git push origin
+
+Set-Location $originalFolder
